4f6a39e99d380792928109ae20a6f335439d372d
Hannah Hwang <hhannah.k.hwang@gmail.com>
2026-05-15 10:36:58 +0000

    ec

--- include/vid.h
+++ include/vid.h
@@ -1,5 +1,7 @@
 #ifndef _VID_H_
 #define _VID_H_
+#include <stdarg.h>
+
 extern int fbuf_init();
 extern int setpos(int r, int c);
 extern unsigned char _binary_font_start[];
@@ -8,6 +10,7 @@ extern char image2_start;
 extern char image3_start;
 extern int color;
 extern int kprintf(char *fmt,...);
+extern int vkprintf(char *fmt, va_list args);
 extern int kpchar(char c, int ro, int co);
 extern int unkpchar(char c, int ro, int co);
 extern int show_bmp(char *p, int start_row, int start_col);
